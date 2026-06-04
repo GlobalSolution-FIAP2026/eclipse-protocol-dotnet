5026c9197b0d42f1b74e3e0c4cd53984070cabe1
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-06-04 14:24:31 -0300

    feat: configurei persistência e documentação da API

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,70 @@
+﻿using GlobalSolution.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlobalSolution.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Localizacao> Localizacoes { get; set; }
+    public DbSet<Propriedade> Propriedades { get; set; }
+    public DbSet<Plantacao> Plantacoes { get; set; }
+    public DbSet<Sensor> Sensores { get; set; }
+    public DbSet<Leitura> Leituras { get; set; }
+    public DbSet<Alerta> Alertas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Usuario>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<Usuario>()
+            .HasMany(u => u.Propriedades)
+            .WithOne(p => p.Usuario)
+            .HasForeignKey(p => p.IdUsuario)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Localizacao>()
+            .HasMany(l => l.Propriedades)
+            .WithOne(p => p.Localizacao)
+            .HasForeignKey(p => p.IdLocalizacao)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Propriedade>()
+            .HasMany(p => p.Plantacoes)
+            .WithOne(p => p.Propriedade)
+            .HasForeignKey(p => p.IdPropriedade)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Plantacao>()
+            .HasMany(p => p.Sensores)
+            .WithOne(s => s.Plantacao)
+            .HasForeignKey(s => s.IdPlantacao)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Sensor>()
+            .HasMany(s => s.Leituras)
+            .WithOne(l => l.Sensor)
+            .HasForeignKey(l => l.IdSensor)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Plantacao>()
+            .HasMany(p => p.Alertas)
+            .WithOne(a => a.Plantacao)
+            .HasForeignKey(a => a.IdPlantacao)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Leitura>()
+            .HasMany(l => l.Alertas)
+            .WithOne(a => a.Leitura)
+            .HasForeignKey(a => a.IdLeitura)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,42 +1,43 @@
+using System.Reflection;
+using GlobalSolution.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseOracle(
+        builder.Configuration.GetConnectionString("OracleConnection")
+    );
+});
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+    options.IncludeXmlComments(
+        Path.Combine(AppContext.BaseDirectory, xmlFilename)
+    );
+
+    options.EnableAnnotations();
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
